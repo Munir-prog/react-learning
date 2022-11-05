96ae5c385124c33ad223cbe66c1641da5ee4ff0d
Munir Zikrullaev <munirprog@gmail.com>
2022-11-05 11:50:29 +0300

    Sorting. Custom Select

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
     const [languages, setLanguages] = useState([
@@ -21,6 +22,8 @@ function App() {
     //     {id: 3, tittle: 'Audi', body: "Description"}
     // ])
 
+    const [selectedSort, setSelectedSort] = useState('')
+
     const createPost = (newPost) => {
         setLanguages([...languages, newPost])
     }
@@ -29,9 +32,27 @@ function App() {
         setLanguages(languages.filter(lang => lang.id !== post.id));
     };
 
+    const sortLanguages = (sort) => {
+        setSelectedSort(sort);
+        setLanguages([...languages].sort((a, b) => a[sort].localeCompare(b[sort])))
+
+    }
+
     return (
         <div className="App">
             <PostForm create={createPost}/>
+            <hr style={{margin: '15px 0'}}/>
+            <div>
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortLanguages}
+                    defaultValue="Sort with"
+                    options={[
+                        {value: 'tittle', name: 'Sort with tittle'},
+                        {value: 'body', name: 'Sort with body'}
+                    ]}
+                />
+            </div>
             {languages.length !== 0
                 ? <PostList posts={languages} remove={removePost} tittle="Some tittle for Languages" />
                 : <h1 style={{textAlign: 'center'}}>Список пустой!</h1>
