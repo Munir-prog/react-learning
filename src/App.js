837eedb6156eeddc9635e40fd143c01ceb7f81de
Munir Zikrullaev <munirprog@gmail.com>
2022-11-05 16:25:20 +0300

    work with api. axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useRef, useState} from 'react'
+import React, {useState} from 'react'
 import './styles/style.css';
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
@@ -6,18 +6,25 @@ import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import {useLanguages} from "./components/hooks/usePosts";
+import axios from "axios";
 
 function App() {
-    const [languages, setLanguages] = useState([
-        {id: 1, tittle: 'Java', body: "OOP language"},
-        {id: 2, tittle: 'Python', body: "Func language"},
-        {id: 3, tittle: 'Bash', body: "Script language"}
-    ])
+    const [languages, setLanguages] = useState([])
 
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
     const sortedAndSearchedPosts = useLanguages(languages, filter.sort, filter.query);
 
+    async function fetchPosts() {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        setLanguages(
+            response.data.map(val => ({
+                id: val.id,
+                tittle: val.title,
+                body: val.body
+            }))
+        )
+    }
     const createPost = (newPost) => {
         setLanguages([...languages, newPost]);
         setModal(false);
@@ -37,8 +44,12 @@ function App() {
             <PostFilter filter={filter} setFilter={setFilter}/>
             <PostList posts={sortedAndSearchedPosts} remove={removePost} tittle="Some tittle for Languages" />
 
-            <MyButton style={{marginTop: '30px'}} onClick={() => setModal(true)}>
+            <MyButton style={{marginTop: '30px', marginRight: '10px'}} onClick={() => setModal(true)}>
                 Add PL
+            </MyButton>
+            {/*<button onClick={fetchPosts}>Get</button>*/}
+            <MyButton style={{marginTop: '30px'}} onClick={() => fetchPosts()}>
+                GetPosts
             </MyButton>
         </div>
     );
